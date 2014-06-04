d49042a5cf27dbd444673bd408bdaf4031ad7412
Chris Wilson <christopher.j.wilson@gmail.com>
2014-06-04 11:53:08 -0500

    doc fix for `implies`

--- lib/bendy/logical.rb
+++ lib/bendy/logical.rb
@@ -9,7 +9,7 @@ module Bendy
     # Material implication
     # `implies` is the common case where if the first thing is true, then pay
     # attention to the second thing. But if the first thing is false then the
-    # whole thing is true: `implies(foo, foo[:bar])`
+    # whole thing is true: `implies(foo){foo[:bar]}`
 
     def implies(p)
       !!p ? yield : true
